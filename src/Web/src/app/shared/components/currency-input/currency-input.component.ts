2d4c56dd0af0509a809c5b54d398e028a352bd53
nagay0410 <nagaiyuuki0410@gmail.com>
2025-12-28 00:21:46 +0900

    feat: initialize Angular 17 project with Angular Material and basic directory structure

---
+++ src/Web/src/app/shared/components/currency-input/currency-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-input',
+  imports: [],
+  templateUrl: './currency-input.component.html',
+  styleUrl: './currency-input.component.css'
+})
+export class CurrencyInputComponent {
+
+}
---
+++ src/Web/src/app/shared/components/member-avatar/member-avatar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-avatar',
+  imports: [],
+  templateUrl: './member-avatar.component.html',
+  styleUrl: './member-avatar.component.css'
+})
+export class MemberAvatarComponent {
+
+}
